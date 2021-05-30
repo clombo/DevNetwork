612eed890f903bdd4578e3290c31b01464609b28
Clombo <christo.lombard@live.co.za>
2021-05-30 10:40:47 +0200

    Auth middleware and user login

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,10 +1,70 @@
 const router = require('express').Router();
+const auth = require('../../middleware/auth');
+const User = require('../../models/User');
+const {check, validationResult} = require('express-validator');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const bcrypt = require('bcryptjs');
 
 // @route   GET api/auth
 // @desc    Test route
 // @access  Public
-router.get('/', (req, res) => {
-   res.json({message: 'auth route'})
+router.get('/', auth, async (req, res) => {
+   try {
+      const user = await User.findById(req.user.id).select('-password');
+      res.json(user);
+   } catch (error) {
+      res.status(500).send('Server error');
+   }
 });
+
+// @route   POST api/auth
+// @desc    Auth user and get token
+// @access  Public
+router.post('', [
+   check('email', 'Valid email is required').isEmail(),
+   check('password','Please enter a password').exists(),
+],async (req, res) => {
+
+   const errors = validationResult(req);
+
+   if(!errors.isEmpty()){
+       return res.status(400).json({errors: errors.array()})
+   }
+
+   const {email, password}  = req.body;
+
+   try {
+      
+      let user = await User.findOne({email});
+
+      if(!user){
+         return res.status(400).json({errors: [{msg: "Invalid credentials!"}]});
+      }
+
+      const isMatch = await bcrypt.compare(password, user.password);
+
+      if(!isMatch){
+         return res.status(400).json({errors: [{msg: "Invalid credentials!"}]});
+      }
+
+      const payload = {
+         user: {id: user.id}
+      }
+     
+     jwt.sign(
+         payload,
+         config.get('secret'),{
+             expiresIn: 360000
+         },(err,token) =>{
+             if(err) throw err;
+             res.json({token})
+         })
+
+   } catch (error) {
+      
+   }
+
+})
 
 module.exports = router;
